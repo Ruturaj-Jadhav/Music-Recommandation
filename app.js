cbf3f0d05fa0d177ed3f1c905522f041b14f3c49
Rutz13 <ruturajjadhav456@gmail.com>
2023-03-04 11:19:27 +0530

    \added sleepy songs

--- app.js
+++ app.js
@@ -10,6 +10,34 @@ app.get('/' , function(req , res){
 res.render('index' , {});
 });
 
+var sleppyarr = []
+
+const fs = require("fs");
+
+// Read users.json file
+fs.readFile("sleepy.json", function(err, data) {
+    // Check for errors
+    if (err) throw err;
+    // Converting to JSON
+    const sleepy = JSON.parse(data);
+
+    for(var i =0 ; i < 10 ;i++){
+      sleppyarr[i] = "https://open.spotify.com/embed/track/"+sleepy[i].id+"?utm_source=generator";
+    }
+});
+
+
+app.get('/sleepy' , function(req , res){
+  console.log(sleppyarr[0])
+  res.render('sleepy' , {sleppyarr : sleppyarr })
+});
+
+
+
+
+
+
+
 app.listen(3000, function(){
   console.log("Server Started");
 });
